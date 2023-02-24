eaf9488837cb7cab96a5d4441ee7b0b16a549392
Suman Das <sum4ndas@gmail.com>
2023-02-24 10:25:13 +0530

    Fibonacci using Iteration.

---
+++ Project1/fibs.js
@@ -0,0 +1,21 @@
+function fibs(num) {
+  let resultArray = [0, 1];
+
+  if (num <= 0 || !Number.isInteger(num)) {
+    return "Please enter a number greater than 0.";
+  }
+  if (num == 1) {
+    return [0];
+  }
+  if (num == 2) {
+    return resultArray;
+  }
+
+  for (let i = 2; i < num; i++) {
+    let arrayLength = resultArray.length;
+    let pushedNum = resultArray[arrayLength - 2] + resultArray[arrayLength - 1];
+    resultArray.push(pushedNum);
+  }
+
+  return resultArray;
+}
